ec69163a3a105052d22dea7e373f836dd9987ee0
guitarbien <guitarbien@gmail.com>
2017-02-01 01:52:22 +0800

    http object 在一次的php life cycle 中應該始終都要使用同一個 object，而不是每次用新的 instance，故使用 Auryn 來反解每個 controller construct 所依賴的 class 為何

---
+++ src/Dependencies.php
@@ -0,0 +1,18 @@
+<?php
+
+$injector = new \Auryn\Injector;
+
+$injector->alias(Http\Request::class, Http\HttpRequest::class);
+$injector->share(Http\HttpRequest::class);
+$injector->define(Http\HttpRequest::class, [
+    ':get'     => $_GET,
+    ':post'    => $_POST,
+    ':cookies' => $_COOKIE,
+    ':files'   => $_FILES,
+    ':server'  => $_SERVER,
+]);
+
+$injector->alias(Http\Response::class, Http\HttpResponse::class);
+$injector->share(Http\HttpResponse::class);
+
+return $injector;
