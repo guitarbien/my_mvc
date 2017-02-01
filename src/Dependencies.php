9a0f396a5c3b3aa9e85ca7f985adeb08859a573c
guitarbien <guitarbien@gmail.com>
2017-02-01 12:14:31 +0800

    (use full class name)

--- src/Dependencies.php
+++ src/Dependencies.php
@@ -2,9 +2,9 @@
 
 $injector = new \Auryn\Injector;
 
-$injector->alias(Http\Request::class, Http\HttpRequest::class);
-$injector->share(Http\HttpRequest::class);
-$injector->define(Http\HttpRequest::class, [
+$injector->alias(\Http\Request::class, \Http\HttpRequest::class);
+$injector->share(\Http\HttpRequest::class);
+$injector->define(\Http\HttpRequest::class, [
     ':get'     => $_GET,
     ':post'    => $_POST,
     ':cookies' => $_COOKIE,
@@ -12,7 +12,7 @@ $injector->define(Http\HttpRequest::class, [
     ':server'  => $_SERVER,
 ]);
 
-$injector->alias(Http\Response::class, Http\HttpResponse::class);
-$injector->share(Http\HttpResponse::class);
+$injector->alias(\Http\Response::class, \Http\HttpResponse::class);
+$injector->share(\Http\HttpResponse::class);
 
 return $injector;
