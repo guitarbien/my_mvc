79aa4a0f317bbe3d095bc542f7cfb24509373361
guitarbien <guitarbien@gmail.com>
2017-02-01 13:51:10 +0800

    controller construct receive renderer, and use file template to presente the view

--- src/Controllers/Homepage.php
+++ src/Controllers/Homepage.php
@@ -2,24 +2,31 @@
 
 namespace App\Controllers;
 
-use \Http\Request;
-use \Http\Response;
+use App\Template\Renderer;
+use Http\Request;
+use Http\Response;
 
 class Homepage
 {
     private $request;
     private $response;
+    private $renderer;
 
-    public function __construct(Request $request, Response $response)
+    public function __construct(Request $request, Response $response, Renderer $renderer)
     {
-        $this->request = $request;
+        $this->request  = $request;
         $this->response = $response;
+        $this->renderer = $renderer;
     }
 
     public function show()
     {
-        $content  = '<h1>Hello World</h1>';
-        $content .= 'Hello ' . $this->request->getParameter('name', 'stranger');
-        $this->response->setContent($content);
+        $data = [
+            'name' => $this->request->getParameter('name', 'stranger'),
+        ];
+
+        $html = $this->renderer->render('Homepage', $data);
+
+        $this->response->setContent($html);
     }
 }
